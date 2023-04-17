57f7eff7ce1f8f2a6a26c5df581851576003ad4e
samdroberts87 <samdroberts87@gmail.com>
2023-04-17 20:51:28 +0100

    yellow box changes

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
+	title: "Fitness for Grownups",
 	url: "https://example.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my fitness journey.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
+		name: "Sam Roberts",
+		email: "samdroberts87@gmail.com",
 		url: "https://example.com/about-me/"
 	}
 }
